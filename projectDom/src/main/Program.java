63748375fbccf63782d0c04f80cb9f69992ebf6e
Aystogon <36318388+Aystogon@users.noreply.github.com>
2018-02-09 21:23:27 -0500

    Add files via upload

---
+++ projectDom/src/main/Program.java
@@ -0,0 +1,10 @@
+package main;
+
+import gui.Window;
+import javafx.application.Application;
+
+public class Program {
+	public static void main(String[] args) {
+		Application.launch(Window.class, args);
+	}
+}
